2dd643dfd4c6bb69a76d5501b06366e4d1fcc6f3
Divine <divinenamirembe@gmail.com>
2024-11-27 23:02:04 +0300

    sign up page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import SignUp from "./signuppage/page";
 
 export default function Home() {
   return (
     <>
-      <h1>Eventura</h1>
+       <SignUp/>
     </>
   );
 }
